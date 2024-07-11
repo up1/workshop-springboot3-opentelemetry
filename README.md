c2a251625272f7f3e69aac4c14db18c8825a3904
up1 <somkiat.p@gmail.com>
2024-07-11 09:19:29 +0700

    Demo spring boot 3 with opentelemetry

---
+++ README.md
@@ -0,0 +1,15 @@
+# Workshop :: Spring Boot 3 + OpenTelemetry
+* Spring Boot 3
+* Opentelemetry
+  * Distributes tracing
+  * Logging
+
+## Steps to run
+```
+$docker compose up -d --build 
+$docker compose ps
+```
+
+List of urls
+* Jaeger :: http://localhost:16686/search
+* API :: http://localhost:8080/hello
---
+++ oteldemo/src/main/java/com/example/oteldemo/HelloController.java
@@ -0,0 +1,36 @@
+package com.example.oteldemo;
+
+import io.opentelemetry.api.GlobalOpenTelemetry;
+import io.opentelemetry.api.trace.Tracer;
+import io.opentelemetry.instrumentation.annotations.WithSpan;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    private static final Tracer tracer = GlobalOpenTelemetry.getTracer("java-service");
+
+    private final HelloService service;
+
+    public HelloController(HelloService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/hello")
+    public String sayHi(){
+        return service.getMessage();
+    }
+
+}
+
+@Service
+class HelloService {
+
+    @WithSpan("HelloService:getMessage")
+    public String getMessage() {
+
+        return "Hello world";
+    }
+}
