bd9ac09a97224edeedf600312895a6688050143a
up1 <somkiat.p@gmail.com>
2024-10-03 23:40:12 +0700

    Add loging with JSON

--- oteldemo/src/main/java/com/example/oteldemo/HelloController.java
+++ oteldemo/src/main/java/com/example/oteldemo/HelloController.java
@@ -2,14 +2,19 @@ package com.example.oteldemo;
 
 import io.opentelemetry.api.GlobalOpenTelemetry;
 import io.opentelemetry.api.trace.Tracer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static net.logstash.logback.argument.StructuredArguments.keyValue;
 
 @RestController
 public class HelloController {
     private static final Tracer tracer = GlobalOpenTelemetry.getTracer("java-service");
-    private final HelloService service;
+    private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
 
+    private final HelloService service;
 
     public HelloController(HelloService service) {
         this.service = service;
@@ -17,6 +22,9 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String sayHi(){
+        logger.info("Default log");
+        logger.info("Add property message {}", keyValue("orderId", 1234));
+
         return service.getMessage();
     }
 
